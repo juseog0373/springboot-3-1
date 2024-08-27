4ec129eaa083dba6e5d01c14633ac3e6523ee818
jskim <juseog0373@naver.com>
2024-08-27 21:42:21 +0900

    week1 commit

---
+++ BookRentalPjt/src/main/java/com/office/library/admin/AdminHomeController.java
@@ -0,0 +1,15 @@
+package com.office.library.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class AdminHomeController {
+	@RequestMapping(value = {"", "/"}, method = GET)
+	public String home() {
+		System.out.println("[AdminHomeController] home()");
+  		return "admin/home";
+	}
+}
